f7589006e39bedcf49ef6f803a59ab3f2149f431
Juan Ignacio <juanimen34@gmail.com>
2022-09-03 14:47:05 -0300

    Subiendo

---
+++ src/exports/images.js
@@ -0,0 +1,8 @@
+import image1 from "../assets/Foto1.jpg"
+import image2 from "../assets/Foto2.jpg"
+import image3 from "../assets/Foto3.jpg"
+import image4 from "../assets/Foto4.jpg"
+import image5 from "../assets/Foto5.jpg"
+import image6 from "../assets/Foto6.jpg"
+
+export default [image1, image2, image3, image4, image5, image6]
